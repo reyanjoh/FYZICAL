2884d33e9be433fe83913a9105c989b6c281804d
Reyan <reyanjohnnarvaja@gmail.com>
2024-10-29 04:41:45 +0800

    api

---
+++ app/api/blogs/route.jsx
@@ -0,0 +1,41 @@
+import Blog from "@/lib/models/blog"
+// import { connect } from "mongoose"
+import {connect} from '@/lib/db'
+import { NextResponse } from "next/server"
+
+export const GET = async () =>{
+
+    // console.log(process.env.MONGODB_URI);
+    
+    try {
+        await connect()
+        const blog = await Blog.find()
+        return new NextResponse(JSON.stringify(blog), {status: 200})
+    } catch (error) {
+        // console.log(error);
+        
+        return new NextResponse('err'+ error.message, {
+            status: 500,
+        })
+    }
+}
+
+export const POST = async (req, res) => {
+    try {
+        const body = await req.json()
+        await connect()
+        const newBlog = new Blog(body)
+        await newBlog.save();
+
+        return new NextResponse(JSON.stringify({
+            message: 'Blog Created', blog: newBlog
+        }),
+        { status:200 }
+    )
+        
+    } catch (error) {
+        return new NextResponse('error: Failed to create new Blog '+ error.message, {
+            status: 500,
+        })
+    }
+}
---
+++ app/api/staffs/route.jsx
@@ -0,0 +1,46 @@
+import Staff from "@/lib/models/staff"
+// import { connect } from "mongoose"
+import {connect} from '@/lib/db'
+import { NextResponse } from "next/server"
+
+export const GET = async () =>{
+
+    // console.log(process.env.MONGODB_URI);
+    
+    try {
+        await connect()
+        const staff = await Staff.find()
+        // const staff = await Staff.findById({_id: { $oid: "6717e8e102b3d874d3871621"}})
+        // const staff = await Staff.findById({_id: "6717e8e102b3d874d3871621"})
+
+
+        return new NextResponse(JSON.stringify(staff), {status: 200})
+    } catch (error) {
+        // console.log(error);
+        
+        return new NextResponse('err'+ error.message, {
+            status: 500,
+        })
+    }
+}
+
+
+export const POST = async (req, res) => {
+    try {
+        const body = await req.json()
+        await connect()
+        const newStaff = new Staff(body)
+        await newStaff.save();
+
+        return new NextResponse(JSON.stringify({
+            message: 'Staff Created', staff: newStaff
+        }),
+        { status:200 }
+    )
+        
+    } catch (error) {
+        return new NextResponse('error: Failed to create new Staff '+ error.message, {
+            status: 500,
+        })
+    }
+}
